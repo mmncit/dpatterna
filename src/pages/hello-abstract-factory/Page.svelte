4f1e853d2393528bac572d5831a2605a0e4d5ac1
mamun <mamun.rashid@seequent.com>
2021-07-29 05:00:49 -0600

    abstract factory pattern

---
+++ src/pages/hello-abstract-factory/Page.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { getIcecream } from "./prouct-provider";
+
+  let selectedMenu = "Choco Blast";
+  let selectedContainer = "Stick";
+  let container = ["Stick", "Cup", "Cone"];
+  let menu = ["Choco Blast", "Sweety Strawberry", "Buttery Butterscotch"];
+
+  $: icecream = getIcecream(selectedMenu, selectedContainer);
+
+  $: src = `./images/icecream-factory/${icecream}.png`;
+
+</script>
+
+<style>
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+  .portrait {
+    height: 300px;
+    width: 500px;
+  }
+
+</style>
+
+<h1>Icecream Abstract Factory</h1>
+
+<h2>Flavours</h2>
+
+<select bind:value={selectedMenu}>
+  {#each menu as flavour}
+    <option value={flavour}>{flavour}</option>
+  {/each}
+</select>
+
+<h2>How you wanna hold your icecream?</h2>
+<select bind:value={selectedContainer}>
+  {#each container as holder}
+    <option value={holder}>{holder}</option>
+  {/each}
+</select>
+
+<p>You ordered {selectedMenu} icecream on {selectedContainer}</p>
+
+<div class="portrait"><img {src} alt={selectedMenu} /></div>
---
+++ src/patterns/factory/abstract-factory-icecream.ts
@@ -0,0 +1,116 @@
+// creator has multiple concrete creators
+// concrete creators produce concrete products
+// concrete products are implemented from product
+
+// product1 interface
+interface Icecream {
+  get(): string; // return the flavor of icecream
+}
+
+// concrete product
+class ChocoIcecream implements Icecream {
+  public get() {
+    return "choco";
+  }
+}
+class ButterscotchIcecream implements Icecream {
+  public get() {
+    return "butterscotch";
+  }
+}
+class StrawberryIcecream implements Icecream {
+  public get() {
+    return "strawberry";
+  }
+}
+
+// product2 interface
+interface IcecreamContainer {
+  getContainedIcecream(icecream: Icecream): string;
+}
+
+// concrete product2
+class StickContainer implements IcecreamContainer {
+  getContainedIcecream(icecream: Icecream): string {
+    console.log(`stick/${icecream.get()}`);
+    return `stick/${icecream.get()}`;
+  }
+}
+
+class CupContainer implements IcecreamContainer {
+  getContainedIcecream(icecream: Icecream): string {
+    return `cup/${icecream.get()}`;
+  }
+}
+
+class ConeContainer implements IcecreamContainer {
+  getContainedIcecream(icecream: Icecream): string {
+    return `cone/${icecream.get()}`;
+  }
+}
+
+// abstract factory
+abstract class IceCreamFactory {
+  public abstract getIcecreamFromfactory(): Icecream;
+  public abstract getContainerfromfactory(): IcecreamContainer;
+  public getIcecream() {
+    const icecream = this.getIcecreamFromfactory();
+    const container = this.getContainerfromfactory();
+    return container.getContainedIcecream(icecream);
+  }
+}
+
+// concrete creators/factories
+export class ChocoStickCart extends IceCreamFactory {
+  public getIcecreamFromfactory(): Icecream {
+    return new ChocoIcecream();
+  }
+  public getContainerfromfactory(): IcecreamContainer {
+    return new StickContainer();
+  }
+}
+
+export class ButterscotchStickCart extends IceCreamFactory {
+  public getIcecreamFromfactory(): Icecream {
+    return new ButterscotchIcecream();
+  }
+  public getContainerfromfactory(): IcecreamContainer {
+    return new StickContainer();
+  }
+}
+
+export class StrawberryStickCart extends IceCreamFactory {
+  public getIcecreamFromfactory(): Icecream {
+    return new StrawberryIcecream();
+  }
+  public getContainerfromfactory(): IcecreamContainer {
+    return new StickContainer();
+  }
+}
+
+export class ChocoCupCart extends IceCreamFactory {
+  public getIcecreamFromfactory(): Icecream {
+    return new ChocoIcecream();
+  }
+  public getContainerfromfactory(): IcecreamContainer {
+    return new CupContainer();
+  }
+}
+
+export class ButterscotchCupCart extends IceCreamFactory {
+  public getIcecreamFromfactory(): Icecream {
+    return new ButterscotchIcecream();
+  }
+  public getContainerfromfactory(): IcecreamContainer {
+    return new CupContainer();
+  }
+}
+
+export class StrawberryCupCart extends IceCreamFactory {
+  public getIcecreamFromfactory(): Icecream {
+    return new StrawberryIcecream();
+  }
+  public getContainerfromfactory(): IcecreamContainer {
+    return new CupContainer();
+  }
+}
