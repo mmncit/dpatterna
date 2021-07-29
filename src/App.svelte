df7a4bf460b0ef29413db9684b01d383ae281b42
mamun <mamun.rashid@seequent.com>
2021-07-28 23:19:30 -0600

    icecream factory

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,12 @@
   import Home from "./pages/Home.svelte";
   import HelloWorld from "./pages/hello-world/Page.svelte";
   import HelloDecorator from "./pages/hello-decorator/Page.svelte";
+  import HelloFactory from "./pages/hello-factory/Page.svelte";
 
   const examples = [
     { title: "hello-world", component: HelloWorld },
     { title: "hello-decorator", component: HelloDecorator },
+    { title: "hello-factory", component: HelloFactory },
   ];
 
   let page;
