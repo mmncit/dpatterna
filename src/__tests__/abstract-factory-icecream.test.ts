5b54232f4b3a58d0607bc71b962c401a8f823df4
Jisan <jishananam@iut-dhaka.edu>
2021-08-01 18:45:04 +0600

    Implemented some test cases for Abstract Factory Method problem

---
+++ src/__tests__/abstract-factory-icecream.test.ts
@@ -0,0 +1,22 @@
+import {ButterscotchConeCart,StrawberryStickCart,ChocoCupCart} from "../patterns/factory/abstract-factory-icecream";
+import {getIcecream} from "../pages/hello-abstract-factory/prouct-provider"
+describe("Icecream Factory", () => {
+    test('Strawberry Ice cream with stick',()=>{
+       let expectation =getIcecream('Sweety Strawberry','Stick')
+        let reality = new StrawberryStickCart()
+        expect(expectation).toEqual(reality.getIcecream())
+
+    })
+    test('Butterscotch Ice cream with cone',()=>{
+       let expectation =getIcecream('Buttery Butterscotch','Cone')
+        let reality = new ButterscotchConeCart()
+        expect(expectation).toEqual(reality.getIcecream())
+
+    })
+    test('Choco Ice cream with cup',()=>{
+        let expectation =getIcecream('Choco Blast','Cup')
+        let reality = new ChocoCupCart()
+        expect(expectation).toEqual(reality.getIcecream())
+    })
+
+})
