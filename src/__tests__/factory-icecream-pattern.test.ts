7037ea354015caaa6006425bd76b03ea0464888b
farzana-snigdha <52860135+farzana-snigdha@users.noreply.github.com>
2021-08-02 14:57:37 +0600

    unit test for factory ice cream

---
+++ src/__tests__/factory-icecream-pattern.test.ts
@@ -0,0 +1,26 @@
+import { getIcecream } from "pages/hello-factory/prouct-provider";
+import {
+  ChocoCart,
+  ButterscotchCart,
+  StrawberryCart,
+} from "patterns/factory/factory-icecream";
+
+describe("Factory Icecream Pattern Test", () => {
+  test("ChocoCart", () => {
+    const expected = getIcecream("Choco Blast");
+    const ans = new ChocoCart().getIcecream();
+    expect(expected).toEqual(ans);
+  });
+
+  test("ButterscotchCart", () => {
+    const expected = getIcecream("Buttery Butterscotch");
+    const ans = new ButterscotchCart().getIcecream();
+    expect(expected).toEqual(ans);
+  });
+
+  test("StrawberryCart", () => {
+    const expected = getIcecream("Sweety Strawberry");
+    const ans = new StrawberryCart().getIcecream();
+    expect(expected).toEqual(ans);
+  });
+});
