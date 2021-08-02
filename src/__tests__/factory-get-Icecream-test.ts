f53aa5c50ba2f7340c91e373a10879caf26a64ea
Labnan <khalidmasum@iut-dhaka.edu>
2021-08-02 15:22:52 +0600

    Change expect (boolean) to expect.toEqual

--- src/__tests__/factory-get-Icecream-test.ts
+++ src/__tests__/factory-get-Icecream-test.ts
@@ -4,17 +4,17 @@ import {getIcecream} from "../pages/hello-factory/prouct-provider";
 describe("Ice-cream Factory Context", () => {
     it("iceCream should be equal to butterscotch", () => {
         const iceCream = getIcecream("Buttery Butterscotch");
-        expect(iceCream==="butterscotch");
+        expect(iceCream).toEqual("butterscotch");
     });
 
     it("iceCream should be equal to strawberry", () => {
         const iceCream = getIcecream("Sweety Strawberry");
-        expect(iceCream==="strawberry");
+        expect(iceCream).toEqual("strawberry");
     });
 
     it("iceCream should be equal to choco", () => {
         const iceCream = getIcecream("Choco Blast");
-        expect(iceCream==="choco");
+        expect(iceCream).toEqual("choco");
     });
 
 });
