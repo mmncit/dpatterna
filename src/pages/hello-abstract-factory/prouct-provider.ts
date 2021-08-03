f3670710cf81f13f09f2846e85723160d3b227e8
Adnan Rahman Robi <55150977+Adnanrobi@users.noreply.github.com>
2021-08-03 23:37:26 +0600

    completed the cone part

--- src/pages/hello-abstract-factory/prouct-provider.ts
+++ src/pages/hello-abstract-factory/prouct-provider.ts
@@ -5,6 +5,9 @@ import {
   ChocoStickCart,
   StrawberryCupCart,
   StrawberryStickCart,
+  ChocoConeCart,
+  ButterscotchConeCart,
+  StrawberryConeCart,
 } from "patterns/factory/abstract-factory-icecream";
 
 function orderHandler(selectedMenu: string, selectedContainer: string) {
@@ -29,6 +32,15 @@ function orderHandler(selectedMenu: string, selectedContainer: string) {
     case "Buttery Butterscotch on Cup":
       return new ButterscotchCupCart();
       break;
+      case "Choco Blast on Cone":
+        return new ChocoConeCart();
+      break;
+      case "Sweety Strawberry on Cone":
+        return new StrawberryConeCart();
+      break;
+      case "Buttery Butterscotch on Cone":
+        return new ButterscotchConeCart();
+        break;
     default:
     // code block
   }
