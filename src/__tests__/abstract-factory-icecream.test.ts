8e345d4c8cc194899faa3343e2283e9250ace618
khairatunhissan <khairatunhissan@gmail.com>
2021-08-03 23:58:37 +0600

    abstract-factory-icecream test case

---
+++ src/__tests__/abstract-factory-icecream.test.ts
@@ -0,0 +1,27 @@
+import {ChocoCupCart,StrawberryStickCart,ButterscotchConeCart} from "../patterns/factory/abstract-factory-icecream";
+import {getIcecream} from "../pages/hello-abstract-factory/prouct-provider"
+
+describe("Abstract factory icecream test", () => {
+    test('Choco icecream on Cup test', () => {
+        let expected = getIcecream('Choco Blast','Cup');
+        let reality = new ChocoCupCart();
+        expect(expected).toEqual(reality.getIcecream());
+    })
+
+    test('Strawberry icecream on Stick test', () => {
+       let expected = getIcecream('Sweety Strawberry','Stick');
+        let reality = new StrawberryStickCart();
+        expect(expected).toEqual(reality.getIcecream());
+    })
+
+    test('Butterscotch icecream on Cone test', () => {
+        let expected = getIcecream('Buttery Butterscotch','Cone');
+        let reality = new ButterscotchConeCart();
+        expect(expected).toEqual(reality.getIcecream());
+    })
+
+    
+
+    
+    
+})
