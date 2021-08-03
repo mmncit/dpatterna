e78faffef96eb129a339ada3a2f10de412c0e352
Siana Rizwan <sianarizwan@gmail.com>
2021-08-03 20:15:49 +0600

    Update factory-design.test.ts

--- src/__tests__/factory-design.test.ts
+++ src/__tests__/factory-design.test.ts
@@ -1,6 +1,8 @@
 import { getIcecream } from "pages/hello-factory/prouct-provider";
 import {
   ChocoCart,
+  ButterscotchCart,
+  StrawberryCart,
 } from "patterns/factory/factory-icecream";
 
 describe("Factory Icecream Pattern Test", () => {
@@ -9,6 +11,18 @@ describe("Factory Icecream Pattern Test", () => {
     const ans = new ChocoCart().getIcecream();
     expect(expected).toEqual(ans);
   })
+
+  test("ButterscotchCart", () => {
+    const expected = getIcecream("Buttery Butterscotch");
+    const ans = new ButterscotchCart().getIcecream();
+    expect(expected).toEqual(ans);
+  });
+
+  test("StrawberryCart", () => {
+    const expected = getIcecream("Sweety Strawberry");
+    const ans = new StrawberryCart().getIcecream();
+    expect(expected).toEqual(ans);
+  });
 
 }
 )
