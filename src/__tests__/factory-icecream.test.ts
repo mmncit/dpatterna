c0482d8daad7126feb93002a030beaf1d625952e
Ratul Mahjabin <ratulmahjabin@iut-dhaka.edu>
2021-08-03 22:04:44 +0600

    test case added for factory icecream

---
+++ src/__tests__/factory-icecream.test.ts
@@ -0,0 +1,26 @@
+import {
+  ChocoCart,
+  ButterscotchCart,
+  StrawberryCart,
+} from '../patterns/factory/factory-icecream'
+import { getIcecream } from '../pages/hello-factory/prouct-provider'
+
+describe('Factory icecream test', () => {
+  test('Choco Blast icecream test', () => {
+    let expected = getIcecream('Choco Blast')
+    let reality = new ChocoCart()
+    expect(expected).toEqual(reality.getIcecream())
+  })
+
+  test('Sweety Strawberry icecream test', () => {
+    let expected = getIcecream('Sweety Strawberry')
+    let reality = new StrawberryCart()
+    expect(expected).toEqual(reality.getIcecream())
+  })
+
+  test('Buttery Butterscotch icecream test', () => {
+    let expected = getIcecream('Buttery Butterscotch')
+    let reality = new ButterscotchCart()
+    expect(expected).toEqual(reality.getIcecream())
+  })
+})
