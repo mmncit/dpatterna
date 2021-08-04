5e70b983ca103f8521cf69f7a6426af729c08b1a
afzal <afzal.siddique99@gmail.com>
2021-08-05 00:49:01 +0600

    reverted

--- src/__tests__/crypto-encoding.test.ts
+++ src/__tests__/crypto-encoding.test.ts
@@ -1,5 +1,5 @@
 import CryptoEncoding from "../utils/crypto-encoding";
-
+// test
 describe("encode and then decode", () => {
   it("encode followed by decode gives original string", () => {
     expect(CryptoEncoding.decode(CryptoEncoding.encode("zzz ZZ  zZ"))).toEqual(
