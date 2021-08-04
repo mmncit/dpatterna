47f550c9a1ab5ee9cd11c4e8a7741695c6442349
Md. Reyanus Salehin <salehin24.rs@gmail.com>
2021-08-05 00:40:02 +0600

    abstract factory ice-cream test added

--- src/__tests__/abstract-factory-icecream.test.ts
+++ src/__tests__/abstract-factory-icecream.test.ts
@@ -1,22 +1,78 @@
-import {ButterscotchConeCart,StrawberryStickCart,ChocoCupCart} from "../patterns/factory/abstract-factory-icecream";
-import {getIcecream} from "../pages/hello-abstract-factory/prouct-provider"
-describe("Icecream Abstract Factory", () => {
-    test('Strawberry Ice cream with stick',()=>{
-       let expectation =getIcecream('Sweety Strawberry','Stick')
-        let reality = new StrawberryStickCart()
-        expect(expectation).toEqual(reality.getIcecream())
+import {
+    ButterscotchStickCart, 
+    ButterscotchCupCart, 
+    ButterscotchConeCart,
+    ChocoStickCart, 
+    ChocoConeCart, 
+    ChocoCupCart 
+    StrawberryStickCart, 
+    StrawberryCupCart, 
+    StrawberryConeCart, 
+} from "../patterns/factory/abstract-factory-icecream";
 
-    })
-    test('Butterscotch Ice cream with cone',()=>{
+import {
+    getIcecream
+} from "../pages/hello-abstract-factory/prouct-provider"
+describe("Icecream Factory", () => {
+
+     test('Butterscotch ice cream on Stick',()=>{
+        let expectation =getIcecream('Buttery Butterscotch','Stick')
+         let reality = new ButterscotchStickCart()
+         expect(expectation).toEqual(reality.getIcecream())
+
+     })
+
+     test('Butterscotch ice cream on Cup',()=>{
+        let expectation =getIcecream('Buttery Butterscotch','Cup')
+         let reality = new ButterscotchCupCart()
+         expect(expectation).toEqual(reality.getIcecream())
+
+     })
+
+    test('Butterscotch ice cream on Cone',()=>{
        let expectation =getIcecream('Buttery Butterscotch','Cone')
         let reality = new ButterscotchConeCart()
         expect(expectation).toEqual(reality.getIcecream())
 
     })
-    test('Choco Ice cream with cup',()=>{
+
+    test('Chocolate ice cream on Stick',()=>{
+        let expectation =getIcecream('Choco Blast','Stick')
+        let reality = new ChocoStickCart()
+        expect(expectation).toEqual(reality.getIcecream())
+    })
+
+    test('Chocolate ice cream on Cup',()=>{
         let expectation =getIcecream('Choco Blast','Cup')
         let reality = new ChocoCupCart()
         expect(expectation).toEqual(reality.getIcecream())
     })
+
+    test('Chocolate ice cream on Cone',()=>{
+        let expectation =getIcecream('Choco Blast','Cone')
+        let reality = new ChocoConeCart()
+        expect(expectation).toEqual(reality.getIcecream())
+    })
+
+    test('Strawberry ice cream on Stick',()=>{
+       let expectation =getIcecream('Sweety Strawberry','Stick')
+        let reality = new StrawberryStickCart()
+        expect(expectation).toEqual(reality.getIcecream())
+
+    })
+
+    test('Strawberry ice cream on Cup',()=>{
+        let expectation =getIcecream('Sweety Strawberry','Cup')
+         let reality = new StrawberryCupCart()
+         expect(expectation).toEqual(reality.getIcecream())
+
+     })
+
+     test('Strawberry ice cream on Cone',()=>{
+        let expectation =getIcecream('Sweety Strawberry','Cone')
+         let reality = new StrawberryConeCart()
+         expect(expectation).toEqual(reality.getIcecream())
+
+     })
 
 })
