f6679a4ac6b9e5f7b894c9a72099e4da2a41273b
Fahim <fahimabrar191336@gmail.com>
2021-08-05 00:32:38 +0600

    abstarct factory test case added

---
+++ src/__tests__/abstract-factory.test.ts
@@ -0,0 +1,25 @@
+import {ChocoCart, ButterscotchCart, StrawberryCart} from "../patterns/factory/factory-icecream";
+import {getIcecream} from "../pages/hello-factory/prouct-provider"
+
+describe("Testing iceCreamFactory", () => {
+    
+    test('Sweety Strawberry', () => {
+        let expected = getIcecream('Sweety Strawberry');
+        let reality = new StrawberryCart();
+        expect(expected).toEqual(reality.getIcecream());
+    })
+    
+    test('Choco Blast', () => {
+        let expected = getIcecream('Choco Blast');
+        let reality = new ChocoCart();
+        expect(expected).toEqual(reality.getIcecream());
+    })
+
+
+    test('Buttery Butterscotch', () => {
+        let expected = getIcecream("Buttery Butterscotch");
+        let reality = new ButterscotchCart();
+        expect(expected).toEqual(reality.getIcecream());
+    })
+
+}) 
