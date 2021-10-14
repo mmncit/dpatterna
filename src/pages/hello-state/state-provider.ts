4b18d5be845b5cb32b9aee60fd321761d3ec4fca
TasfiaPrima <55846643+TasfiaPrima@users.noreply.github.com>
2021-10-14 22:59:40 +0600

    lab task done

--- src/pages/hello-state/state-provider.ts
+++ src/pages/hello-state/state-provider.ts
@@ -1,14 +1,79 @@
-export function getState(message: string) {
-  switch (message) {
+export function getState (action: String, gate: Gate): Gate{
+  switch (action){
     case "enter":
-      return "open";
-
+      gate.enter()
+      return gate
     case "payOk":
-      return "open";
-
+      gate.payOk()
+      return gate
     case "payFailed":
-      return "closed";
+      gate.payFailed()
+      return gate
     default:
-      return "processing";
+      gate.payFailed()
+      return gate
+  }
+}
+export interface GateState{
+  enter():void
+  payOk(): void
+  payFailed(): void
+  state(): String
+}
+export class OpenGateState implements GateState{
+  gate: Gate
+  public constructor(g: Gate){
+    this.gate = g
+  }
+  enter():void{
+    this.gate.changeState(new ClosedGateState(this.gate))
+  }
+  payOk(): void{
+
+  }
+  payFailed(): void{
+
+  }
+  state(): String{
+    return `open`
+  }
+}
+export class ClosedGateState implements GateState{
+  gate: Gate
+  public constructor(g: Gate){
+    this.gate = g
+  }
+  enter():void{
+
+  }
+  payOk(): void{
+    this.gate.changeState(new OpenGateState(this.gate))
+  }
+  payFailed(): void{
+
+  }
+  state(): String{
+    return `closed`
+  }
+}
+export class Gate{
+  state: GateState
+  public constructor(){
+    this.state = new ClosedGateState(this)
+  }
+  public enter(): void{
+    this.state.enter()
+  }
+  public payOk(): void {
+    this.state.payOk()
+  }
+  public payFailed(): void{
+    this.state.payFailed()
+  }
+  public getState(): String{
+    return this.state.state()
+  }
+  public changeState(state: GateState): void {
+    this.state = state
   }
 }
